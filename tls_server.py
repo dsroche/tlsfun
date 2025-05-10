aabdbc806dde9f802be4c2d99e7a1f39969dcd07
Daniel S. Roche <roche@usna.edu>
2025-05-10 09:04:10 -0400

    basic tls server working

--- tls_server.py
+++ tls_server.py
@@ -2,21 +2,38 @@
 
 from secrets import SystemRandom
 from random import Random
+from threading import Thread
+import socket
 
+from spec import kwdict, UnpackError
 from tls_common import *
 from tls13_spec import (
     ServerState,
+    Version,
+    Handshake,
+    HandshakeType,
+    ExtensionType,
+    ContentType,
 )
 from tls_records import (
     Connection,
+    HandshakeBuffer,
 )
 from tls_keycalc import (
     KeyCalc,
+    HandshakeTranscript,
+)
+from tls_crypto import (
+    gen_cert,
+    get_hash_alg,
+    get_cipher_alg,
+    get_sig_alg,
+    get_kex_alg,
 )
 
 class Server(Connection):
-    def __init__(self, cert_secrets, rseed=None)
-        super().__init__(self, None, _ServerHandshake(cert_secrets, rseed))
+    def __init__(self, cert_secrets, rseed=None):
+        super().__init__(None, _ServerHandshake(cert_secrets, rseed))
 
 class _ServerHandshake:
     def __init__(self, cert_secrets, rseed):
@@ -28,6 +45,7 @@ class _ServerHandshake:
         self._sh_exts = []
         self._ee_exts = []
         self._exts_received = set()
+        self._kex_modes = set()
 
     @property
     def started(self):
@@ -51,7 +69,8 @@ class _ServerHandshake:
         self._rreader.hs_buffer = HandshakeBuffer(self)
         self._rwriter = rwriter
 
-    def _send_hs_msg(self, typ, vers=Version.TLS_1_2, raw):
+    def _send_hs_msg(self, typ, vers=Version.TLS_1_2, raw=None):
+        assert raw is not None
         logger.info(f"sending hs message {typ} to client")
         self._rwriter.send(
             typ     = ContentType.HANDSHAKE,
@@ -92,7 +111,8 @@ class _ServerHandshake:
                 self._cipher = get_cipher_alg(csuite)
             except ValueError:
                 continue
-            self._cipher_suite = csuite
+            logger.info(f'negotiated cipher suite {csuite}')
+            self._key_calc.cipher_suite = csuite
             break
         else:
             raise TlsError(f"no supported cipher suites in {body.ciphers}")
@@ -105,6 +125,8 @@ class _ServerHandshake:
         if ExtensionType.KEY_SHARE not in self._exts_received:
             # TODO allow for PSK-only mode
             raise TlsError("client did not provide a key exchange share")
+        # TODO allow psks
+        self._key_calc.psk = None
 
         self._state = ServerState.NEGOTIATED
 
@@ -115,7 +137,7 @@ class _ServerHandshake:
             body = kwdict(
                 server_random = self._rgen.randbytes(32),
                 session_id    = body.session_id,
-                cipher_suite  = self._cipher_suite,
+                cipher_suite  = self._key_calc.cipher_suite,
                 extensions    = self._sh_exts,
             ),
         )
@@ -183,7 +205,17 @@ class _ServerHandshake:
                             self._key_calc.server_application_traffic_secret)
         logger.info(f'switched to application key for sending')
 
+        # TODO handle 0-RTT early data here
+
+        self._rreader.rekey(self._cipher, self._hash_alg,
+                            self._key_calc.client_handshake_traffic_secret)
+        logger.info(f'switched to handshake key for receiving')
+
         self._state = ServerState.WAIT_FLIGHT2
+
+        # TODO handle client auth here
+
+        self._state = ServerState.WAIT_FINISHED
 
 
     def _process_client_ext(self, ext):
@@ -193,26 +225,23 @@ class _ServerHandshake:
             case ExtensionType.SUPPORTED_VERSIONS:
                 if Version.TLS_1_3 not in ext.data:
                     raise TlsError("client does not support TLS 1.3")
+                logger.info('negotiated TLS 1.3')
                 self._sh_exts.append(kwdict(
                     typ = ExtensionType.SUPPORTED_VERSIONS,
                     data = Version.TLS_1_3,
                 ))
             case ExtensionType.SERVER_NAME:
-                logger.info(f"Client sent SNI with hostname '{ext.data.host_name}'")
+                logger.info(f"Client sent SNI with hostnames '{[ent.host_name for ent in ext.data]}'")
             case ExtensionType.SIGNATURE_ALGORITHMS:
                 if self._cert_secrets.sig_alg not in ext.data:
                     raise TlsError(f"client doesn't support sig {self._cert_secrets.sig_alg}")
+                logger.info(f'negotiated sig alg {self._cert_secrets.sig_alg}')
             case ExtensionType.SUPPORTED_GROUPS:
-                for grp in ext.data:
-                    try:
-                        get_kex_alg(grp)
-                    except ValueError:
-                        continue
-                    break
-                else:
-                    raise TlsError(f"no supported groups in {ext.data}")
+                logger.info(f'server ignoring supported groups extension, will just look in key share')
             case ExtensionType.PSK_KEY_EXCHANGE_MODES:
-                raise TlsTODO("server doesn't support PSKs yet") #TODO
+                for mode in ext.data:
+                    logger.info(f'client allows kex mode {mode}')
+                    self._kex_modes.add(mode)
             case ExtensionType.TICKET_REQUEST:
                 raise TlsTODO("server doesn't support PSKs yet") #TODO
             case ExtensionType.KEY_SHARE:
@@ -229,6 +258,7 @@ class _ServerHandshake:
                             pubkey = kex_alg.get_public(kex_private),
                         ),
                     ))
+                    logger.info(f'negotiated kex alg {group}')
                     self._key_calc.kex_secret = kex_alg.exchange(kex_private, pubkey)
                     break
                 else:
@@ -236,11 +266,49 @@ class _ServerHandshake:
             case ExtensionType.PRE_SHARED_KEY:
                 raise TlsTODO("server doesn't support PSKs yet") #TODO
             case _:
-                raise TlsError(f"received unrecognized client extension {ext.typ}")
-
-
+                logger.info(f'IGNORING unrecognized extension {ext.typ}')
 
 
     def _process_finished(self, body):
-        # FIXME TODO
-        raise TlsTODO("not here yet")
+        if body != self._key_calc.client_finished_verify:
+            raise TlsError("client finished has incorrect verify string")
+        logger.info('received correct CF; rekeying to complete handshake')
+        self._rreader.rekey(self._cipher, self._hash_alg,
+                            self._key_calc.client_application_traffic_secret)
+        self._state = ServerState.CONNECTED
+
+
+def serve_once(hostname='localhost', port=5000, cert_secrets=None, rseed=None):
+    """Starts a server that accepts exactly one connection.
+
+    Returns the connected Server object.
+    """
+    if cert_secrets is None:
+        logger.info('generating new self-signed server cert')
+        cert_secrets = gen_cert(hostname)
+
+    with socket.create_server((hostname, port)) as ssock:
+        logger.info(f'listening for connection to {hostname} on port {port}')
+        sock, addr = ssock.accept()
+        logger.info(f'got a connection from {addr}')
+        server = Server(cert_secrets, rseed)
+        server.connect_socket(sock)
+        return server
+
+
+def start_server(hostname='localhost', port=5000, cert_secrets=None, rseed=None):
+    if cert_secrets is None:
+        logger.info('generating new self-signed server cert')
+        cert_secrets = gen_cert(hostname)
+
+    with socket.create_server((hostname, port)) as ssock:
+        while True:
+            logger.info(f'listening for connection to {hostname} on port {port}')
+            sock, addr = ssock.accept()
+            logger.info(f'got a connection from {addr}')
+            newserv = Server(cert_secrets, rseed)
+            if rseed is not None:
+                rseed += 1
+            sthread = Thread(target=newserv.connect_socket, args=(sock,))
+            logger.info(f'launching new thread to handle client connection')
+            sthread.start()
